161fee16d16d8fa918ad6a6906e181df078c61d0
Your Name <you@example.com>
2024-03-27 15:13:43 +0800

    test

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -27,6 +27,11 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if (!IClassroomV2(msg.sender).isEnrolled()) {
+            return 10000;
+        } else {
+            return 123;
+        }
     }
 }
 
